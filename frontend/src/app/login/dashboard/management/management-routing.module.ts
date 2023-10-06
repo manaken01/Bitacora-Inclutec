fee1d29d58834931f82f96b7e92d7a1e2123b8cd
manaken01 <82286286+manaken01@users.noreply.github.com>
2023-10-05 20:19:30 -0600

    role list

--- frontend/src/app/login/dashboard/management/management-routing.module.ts
+++ frontend/src/app/login/dashboard/management/management-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ManagementComponent } from "./management.component";
+import { RoleListComponent } from './login/dashboard/management/role-list/role-list.component';
 
 const routes: Routes = [
   {
@@ -17,5 +18,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
+  declarations: [RoleListComponent, RoleListComponent],
 })
 export class SearchRoutingModule {}
